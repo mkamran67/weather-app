8b63e90963b9709faa23075cdc004a0bf6815279
Muhammad Kamran <mkamran1967@gmail.com>
2020-08-17 16:03:48 -0700

    Fin

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,59 @@
 //  4. Load relevent City Weather information
 
 //  Variables
+let initial = true;
 
 // Run Function on document ready
 $('document').ready(() => {
-  $('#btnSubmit').on('click', submitInput);
+  let clientCity = '';
+
+  $('#btnSubmit').on('click', controllerFunc);
+  $('#citySearch').keypress((e) => {
+    if (e.which === 13) {
+      submitInput();
+      e.preventDefault();
+    }
+  });
+
+  $('#searchHistory').on('click', historyClick);
+
+  if (localStorage.getItem('citylist') !== null) {
+    displayHistoryList();
+  }
+
+  if (initial) {
+    submitInput();
+  }
 });
 
+function historyClick(e) {
+  if (e.target.innerText !== null) {
+    submitInput(e.target.innerText);
+  }
+}
+
+async function initGeo() {
+  let data = '';
+  await $.ajax({
+    method: 'GET',
+    url: 'http://ip-api.com/json/',
+  }).then((res) => {
+    data = res.city;
+  });
+  return data;
+}
+
+function controllerFunc(e) {
+  e.preventDefault();
+  submitInput();
+}
+
 // Submit City Function
-async function submitInput(e, cityName = '') {
+async function submitInput(cityName = '') {
+  if (initial) {
+    cityName = await initGeo();
+  }
+  console.log(cityName);
   let regSpace = new RegExp(/^ /);
   // Will hold weather data
   let weatherObj = {};
@@ -34,25 +79,12 @@ async function submitInput(e, cityName = '') {
 
     // Send Name and Weather Object
     setDisplay(cityCoordinatesString.trueName, weatherObj);
-  } else {
-    console.log(`Empty`);
-  }
-
-  // If not Empty or Space
-  if (cityName !== '' && regSpace.test(cityName) === true) {
-    // Get coordinates
-    let cityCoordinatesString = await getCoordstring(cityName);
-
-    weatherObj = await getWeather(cityCoordinatesString.mainQString);
-
-    setDisplay(cityCoordinatesString.trueName, weatherObj);
+    initial = false;
   } else {
     console.log(`Empty`);
   }
 }
 async function getCoordstring(city) {
-  console.log(`coooordinates`);
-
   let mainQString = ``,
     trueName = '';
   let cordinatesUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=1a0eaaf9b7dc51f4a1835f73728c07a2`;
@@ -67,6 +99,7 @@ async function getCoordstring(city) {
     })
     .fail(() => {
       console.log(`Failed getting coordinates.`);
+      alert(`Invalid name ${city}`);
     });
 
   // Return an object containing the string and the "correct" name
@@ -95,26 +128,76 @@ async function getWeather(qString) {
   return response !== null ? response : false;
 }
 
-function setDisplay(name, weatherObj) {
-  // Set Main Information
-  $('#cityDate').text(`${name} ${moment().format('DD MM YYYY')}`);
-}
+function setDisplay(name, { current, daily }) {
+  // Set Icon corresponding to current conditions
+  let iconUrl = `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png`;
 
-function setLocalStorage(cityName) {
-  // Adds name to array.
-  let temp = [];
+  $('#cityDate').html(
+    `
+    ${name} ${moment().format('[(]MM[/]DD[/]YYYY[)]')}
+    <i>
+        <img src='${iconUrl}' alt='${current.weather[0].main}' />
+    </i>
+    `
+  );
 
-  if (getLocalStorage() !== null) {
-    temp = getLocalStorage();
-    // Check for duplicates
-    // TODO:
-    // if(temp.indexOf(cityName) !== -1){
-    // }
+  $('#temperature span').text(current.temp);
+  $('#humidity span').text(current.humidity);
+  $('#windSpeed span').text(current.wind_speed);
+  $('#uvIndex span').text(current.uvi);
+  if (current.uvi > 8) {
+    $('#uvIndex span').css('background-color', 'red');
+  } else if (current.uvi > 6) {
+    $('#uvIndex span').css('background-color', 'orange');
+    $('#uvIndex span').css('color', 'black');
+  } else if (current.uvi > 3) {
+    $('#uvIndex span').css('background-color', 'green');
+    $('#uvIndex span').css('color', 'white');
+  } else if (current.uvi > 0) {
+    $('#uvIndex span').css('background-color', '#33808D');
   }
 
-  temp.push(cityName);
+  let cardList = ``;
 
-  localStorage.setItem('citylist', JSON.stringify(temp));
+  // Build List
+  for (let i = 0; i < 5; i++) {
+    cardList += `
+    <li class="card">
+        <p>${moment(daily[i].dt * 1000).format('M[/]DD[/]YYYY')}</p>
+        <img src='http://openweathermap.org/img/wn/${
+          daily[i].weather[0].icon
+        }@2x.png' alt='${daily[i].weather[0].main}'/>
+        <p>Temp: <span>${daily[i].temp.day}</span> °F</p>
+        <p>Humidity: <span>${daily[i].humidity}</span>%</p>
+    </li>
+    `;
+  }
+
+  // Append List
+  $('#forecastList').html(cardList);
+
+  // Push City Name to Localstorage + history list
+  setStorageAndDisplay(name);
+}
+
+function setStorageAndDisplay(cityName) {
+  // Adds name to array.
+  let arr = [];
+
+  if (getLocalStorage() !== null && getLocalStorage().length > 0) {
+    arr = [...new Set(getLocalStorage())];
+    if (arr.indexOf(cityName) !== -1) {
+      arr.splice(arr.indexOf(cityName), 1);
+    }
+  }
+
+  arr.push(cityName);
+
+  // Send array of city names to localstorage
+  localStorage.setItem('citylist', JSON.stringify(arr));
+
+  // Display array of
+  displayHistoryList();
 }
 // Returns null if nothing in storage
 function getLocalStorage() {
@@ -122,4 +205,17 @@ function getLocalStorage() {
   return localStorage.getItem('citylist') !== null
     ? JSON.parse(localStorage.getItem('citylist'))
     : null;
+}
+
+function displayHistoryList() {
+  let historyList = getLocalStorage();
+
+  // Clean Previous List
+
+  $('#searchHistory').empty();
+
+  // Append List
+  historyList.forEach((element) => {
+    $('#searchHistory').append(`<li><button>${element}</button></li>`);
+  });
 }
